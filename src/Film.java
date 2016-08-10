3f02aa7e842025ecb9c54afd751935c71b2ec570
alexey2140 <alexqac60m@gmail.com>
2016-08-10 22:41:22 +0300

    repository creation

---
+++ src/Film.java
@@ -0,0 +1,11 @@
+
+public class Film {
+    private String title;
+    private String studio;
+    private String year;
+    private String genre;
+    private String length;
+    private String tagline;
+    private String country;
+
+}
